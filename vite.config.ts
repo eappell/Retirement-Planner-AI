be6f7e7ab9183b4f015de6fb3cbb5922a2882688
Eddie Appell <webdev@appell.com>
2025-11-14 15:25:52 -0800

    fix: Finalize vite config for Vercel environment

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,14 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
+    // const env = loadEnv(mode, '.', '');
     return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
       base: '/',
       plugins: [react()],
       define: {
-        'process.env.API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY)
+        'process.env.API_KEY': JSON.stringify(process.env.VITE_GEMINI_API_KEY)
+        //'process.env.API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY),
+        //'process.env.GEMINI_API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY)
       },
       resolve: {
         alias: {
