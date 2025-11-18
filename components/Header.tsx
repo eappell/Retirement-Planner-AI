ed006cbf45ac40fd924f7da81fb3cf0b0dea9d1c
Eddie Appell <webdev@appell.com>
2025-11-18 06:46:40 -0800

    changed print report button to print

--- components/Header.tsx
+++ components/Header.tsx
@@ -84,7 +84,7 @@ export const Header: React.FC<HeaderProps> = ({
                 </button>
                 <button type="button" onClick={handlePrint} className="flex items-center space-x-2 text-sm text-gray-600 hover:text-brand-primary transition-colors font-medium p-2 rounded-md hover:bg-gray-100">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z" /></svg>
-                    <span>Print Report</span>
+                    <span>Print</span>
                 </button>
 
                     <div className="relative" ref={scenarioMenuRef}>
